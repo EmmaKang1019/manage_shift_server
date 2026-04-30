223611fe0128559387ef9591980cc756fb1ed35d
Kang MinJoo <sht000826@gmail.com>
2026-04-29 22:19:11 -0700

    tree 1

--- managingShift/src/main/java/com/example/shift/ShiftApplication.java
+++ managingShift/src/main/java/com/example/shift/ShiftApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.shift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ShiftApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ShiftApplication.class, args);
 	}
 
 }
--- managingShift/src/test/java/com/example/shift/ShiftApplicationTests.java
+++ managingShift/src/test/java/com/example/shift/ShiftApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.shift;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ShiftApplicationTests {
 
 	@Test
 	void contextLoads() {
